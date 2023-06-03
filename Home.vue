89a590ad43a09548bf48c7cca8694529fc48982e
LanceFrancisco1231 <131014408+LanceFrancisco1231@users.noreply.github.com>
2023-06-03 19:49:34 +0800

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-content> 
+      Home
+    </v-content>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      items: [
+      ],
+    };
+  },
+};
+</script>
