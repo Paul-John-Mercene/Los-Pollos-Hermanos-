ddbd602063d1157d4c5f734727faab51fe62ae20
Paul John Mercene <130619870+Paul-John-Mercene@users.noreply.github.com>
2023-06-07 18:00:47 +0800

    Update About.vue

--- About.vue
+++ About.vue
@@ -1,7 +1,7 @@
 <template>
   <v-content>
     <div class="staticHero">
-      <v-img src="../assets/images/img13.jpg">
+      <v-img src="../assets/images/clouds.jpg">
         <v-row align="end" class="lightbox white--text pa-2 fill-height">
           <v-col>
             <v-container>
@@ -45,37 +45,37 @@ export default {
           id: 1,
           src: require("../assets/images/masubay.jpg"),
           name: 'Karl Steven Masubay',
-          title: 'Director'
+          
         },
         {
           id: 2,
           src: require("../assets/images/sia.jpg"),
           name: 'Jazzmyl Sia',
-          title: 'Photographer'
+          
         },
         {
           id: 3,
           src: require("../assets/images/alagos.jpg"),
           name: 'Daniel Joseph Alagos',
-          title: 'Freelancer'
+          
         },
         {
           id: 4,
           src: require("../assets/images/francisco.jpg"),
           name: 'Lance Francisco',
-          title: 'Freelancer'
+          
         },
         {
           id: 5,
           src: require("../assets/images/colarat.jpg"),
           name: 'Elizalde Colarat Jr.',
-          title: 'Freelancer'
+          
         },
         {
           id: 6,
           src: require("../assets/images/mercene.jpg"),
           name: 'Paul John Mercene',
-          title: 'Freelancer'
+          
         }
       ]
     };
