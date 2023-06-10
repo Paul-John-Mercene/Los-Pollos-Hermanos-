a02ccae53c8d87c36bedd52cc4f92e64604456b8
Jazzmyll Sia <127032860+Jazzmyll@users.noreply.github.com>
2023-06-10 10:36:43 +0800

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,43 +1,49 @@
 <template>
-  <v-carousel>
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    >
-      <v-row class="fill-height" align="center" justify="center">
-        <div style="font-size: 100px; color: white;">{{ item.title }}</div>
-      </v-row>
-    </v-carousel-item>
-  </v-carousel>
+    <div class="heroBlock">
+      <v-carousel cycle>
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition">
+          <v-row
+            class="fill-height title hidden-xs-only"
+            align="center"
+            justify="center"
+          >
+            <div>{{ item.title }}</div>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'MyHero',
-  data() {
-    return {
-      items: [
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          title: 'Los Pollos Hermanos',
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          title: 'Alagos, Colarat',
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          title: 'Masubay, Mercene',
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          title: 'Francisco, Sia',
-        },
-      ],
-    };
-  },
-};
+  export default {
+    name: 'Hero',
+
+    data () {
+      return {
+        items: [
+          {
+            src: require("../assets/images/car1.jpg"),
+            title: "LOS"
+          },
+          {
+            src: require("../assets/images/car2.jpg"),
+            title: "POLLOS"
+          },
+          {
+            src: require("../assets/images/car3.jpg"),
+            title: "HERMANOS"
+          },
+          {
+            src: require("../assets/images/car4.jpg"),
+            title: "PHOTOGRAPHY"
+          }
+        ],
+      }
+    },
+  }
 </script>
