25345f528fb1cd1a9e8379cf42ec47170506998c
Elizalde Colarat <114283245+ecolarat@users.noreply.github.com>
2023-06-14 10:03:40 +0800

    Update Home.vue

--- Home.vue
+++ Home.vue
@@ -1,17 +1,23 @@
 <template>
-    <v-content> 
-      Home
-    </v-content>
+  <v-content>
+    <Hero />
+    <Gallery />
+    <LatestPost />
+  </v-content>
 </template>
 
 <script>
+import Hero from "../components/Hero";
+import Gallery from "../components/Gallery";
+import LatestPost from "../components/LatestPost";
+
 export default {
-  name: 'Home',
-  data() {
-    return {
-      items: [
-      ],
-    };
-  },
+  name: "Home",
+
+  components: {
+    Hero,
+    Gallery,
+    LatestPost
+  }
 };
 </script>
